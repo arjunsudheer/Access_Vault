45fe976e2a1d8d33ad2d90be11ac1e30191043e6
arjunsudheer <arjunsudheer05@outlook.com>
2023-09-16 13:08:06 -0700

    Add ability to control file access
    Change file owner and r, w, x permissions for user, group, and other

--- access_vault.md
+++ access_vault.md
@@ -10,6 +10,27 @@ The access_vault.sh file will automatically update user and file permissions for
 **Required parameter:** The absolute path to the csv file.
 
 * Create a file report file containing a record of what file permissions were updated. Writes the user that ran the script and when the script was run to the first line of the file.
+* Check if the owner of the file is the one specified in the csv file. If not, then update the user of the file to the one specified in the csv file. Log changes in fileReports file.
+* Call the changeFilePermissions() function for the user access level, group access level, and other access level.
+
+
+## changeFilePermissions() function
+This function exists only to simplify the process of updating read, write, and execute permissions for files. This function is called from the manageFiles() function.
+
+**Required parameters:** The absolute path to the file having its permissions changed, access level (r/w/x/- characters from csv file), access level name (user/group/other), the character to
+    use for updates (u/w/r), position of the access rights (u=0, w=1, x=2), and the fileReports file
+
+* Checks if the character 'r' is included.
+    * If it is and the appropriate access level does not have read permissions, then add read permissions for the appropriate access level. Log changes in fileReports file.
+    * If it is not and the appropriate access level has read permissions, then remove the read permissions for the appopriate access level. Log changes in fileReports file.
+
+* Checks if the character 'w' is included.
+    * If it is and the appropriate access level does not have write permissions, then add write permissions for the appropriate access level. Log changes in fileReports file.
+    * If it is not and the appropriate access level has write permissions, then remove the write permissions for the appopriate access level. Log changes in fileReports file.
+
+* Checks if the character 'x' is included.
+    * If it is and the appropriate access level does not have execute permissions, then add execute permissions for the appropriate access level. Log changes in fileReports file.
+    * If it is not and the appropriate access level has execute permissions, then remove the execute permissions for the appopriate access level. Log changes in fileReports file.
 
 
 ## manageUsers() function
--- access_vault.sh
+++ access_vault.sh
@@ -6,11 +6,85 @@ manageFiles() {
         exit
     fi
 
-    fileReport="file_report_$(date +"%H:%M:%S").txt"
-    echo "$(whoami) ran this script to update file permissions on $(date "+%m-%d-%Y at %H:%M:%S")" >> $fileReport
+    local fileUpdateReport="file_report_$(date +"%H:%M:%S").txt"
+    echo "$(whoami) ran this script to update file permissions on $(date "+%m-%d-%Y at %H:%M:%S")" >> $fileUpdateReport
 
-    setupReports $1 "files" $fileReport
+    IFS=","
+    while read -r absoluteFilePath fileOwner userAccess groupAccess otherAccess; do
+        # check file ownership (3rd field in ls -l command)
+        username=$(ls -l $1 | awk '{print $3}')
+        if [[ $fileOwner != $username ]]; then
+            chown $fileOwner $absoluteFilePath
+            echo "Changed owner of $absoluteFilePath to $fileOwner" >> $fileUpdateReport
+        fi
+        # check user access
+        changeFilePermissions $absoluteFilePath $userAccess "user" "u" 0 $fileUpdateReport
+        # check group access
+        changeFilePermissions $absoluteFilePath $groupAccess "group" "g" 1 $fileUpdateReport
+        # check other access
+        changeFilePermissions $absoluteFilePath $otherAccess "other" "o" 2 $fileUpdateReport
+    done < <(tail -n +2 $1)
+
+    setupReports $1 "files" $fileUpdateReport
 }
+
+
+# Helper functions
+changeFilePermissions() {
+    # expects abolute file path as first parameter, access level (from csv file)
+    # access level name (user/group/other) as second parameter, the character to
+    # use for updates (u/w/r), position of the access rights (u=0, w=1, x=2), and
+    # the fileReports file to append change messages to
+    if [[ -z $1 || -z $2 || -z $3 || -z $4 || -z $5 || -z $6 ]]; then
+        return
+    fi
+
+    # Check for read permissions
+    if [[ "$2" == *"r"* ]]; then
+        # pipe the ls -l command to the cut command to check for lack of read permissions
+        if [[ $(ls -l $1 | cut -c $(( $5 * 3 + 2 ))-$(( $5 * 3 + 2 ))) == "-" ]]; then
+            chmod $4+r $1
+            echo "Added read access to $3 for $1" >> $6
+        fi
+    else
+        # pipe the ls -l command to the cut command to check for read permissions
+        if [[ $(ls -l $1 | cut -c $(( $5 * 3 + 2 ))-$(( $5 * 3 + 2 ))) == "r" ]]; then
+            chmod $4-r $1
+            echo "Removed read access from $3 for $1" >> $6
+        fi
+    fi
+
+    # check for write permissions
+    if [[ "$2" == *"w"* ]]; then
+        # pipe the ls -l command to the cut command to check for lack of write permissions
+        if [[ $(ls -l $1 | cut -c $(( $5 * 3 + 3 ))-$(( $5 * 3 + 3 ))) == "-" ]]; then
+            chmod $4+r $1
+            echo "Added write access to $3 for $1" >> $6
+        fi
+    else
+        # pipe the ls -l command to the cut command to check for write permissions
+        if [[ $(ls -l $1 | cut -c $(( $5 * 3 + 3 ))-$(( $5 * 3 + 3 ))) == "w" ]]; then
+            chmod $4-r $1
+            echo "Removed write access from $3 for $1" >> $6
+        fi
+    fi
+
+    # check for execute permissions
+    if [[ "$2" == *"x"* ]]; then
+        # pipe the ls -l command to the cut command to check for lack of execute permissions
+        if [[ $(ls -l $1 | cut -c $(( $5 * 3 + 4 ))-$(( $5 * 3 + 4 ))) == "-" ]]; then
+            chmod $4+r $1
+            echo "Added execute access to $3 for $1" >> $6
+        fi
+    else
+        # pipe the ls -l command to the cut command to check for execute permissions
+        if [[ $(ls -l $1 | cut -c $(( $5 * 3 + 4 ))-$(( $5 * 3 + 4 ))) == "x" ]]; then
+            chmod $4-r $1
+            echo "Removed execute access from $3 for $1" >> $6
+        fi
+    fi
+}
+
 
 manageUsers() {
     # verify that a csv file was given to run this script with
@@ -33,7 +107,7 @@ setupReports() {
 
     local csvFile=$1
     local reportType=$2
-    local fileReport=$3
+    local report=$3
 
     if [[ -f ".reports_path.txt" ]]; then
         reportPath=$(< .reports_path.txt)
@@ -68,13 +142,14 @@ setupReports() {
     fi
 
     # append the file report to the directory marking the time of this script being run, do not overwrite existing files
-    mv -n $fileReport $timeReports
+    mv -n $report $timeReports
 
     cp $csvFile $timeReports/$reportType.csv.bak
 
     # don't give write access to anyone so that the access_vault_reports directory and subdirectores/files cannot be deleted or modified
-    chmod u=rx,g=rx,o= $reportsDirectory $dayReports $timeReports $timeReports/$fileReport $timeReports/$reportType.csv.bak
+    chmod u=rx,g=rx,o= $reportsDirectory $dayReports $timeReports $timeReports/$report $timeReports/$reportType.csv.bak
 }
+
 
 # cd to the directory where this script is located in
 cd $(dirname "$(realpath $0)")
